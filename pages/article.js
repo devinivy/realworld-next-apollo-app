f1c5d16f0d37ffc054e5e04910ec275325fb70f7
Devin Ivy <devin@bigroomstudios.com>
2018-12-19 00:41:23 -0500

    Begin displaying gql results in markup, markdown support

--- pages/article.js
+++ pages/article.js
@@ -1,13 +1,19 @@
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withRouter } from 'next/router';
+import marked from 'marked';
 import Layout from '../components/Layout';
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
-import { withRouter } from 'next/router'
 
 const GET_ARTICLE = gql`
-    query getArticle($slug: ID!) {
+    query ($slug: ID!) {
         article(slug: $slug) {
             slug
-            author { username }
+            title
+            body
+            author {
+                username
+                image
+            }
         }
     }
 `;
@@ -15,39 +21,41 @@ const GET_ARTICLE = gql`
 export default withRouter(({ router }) => (
     <Layout>
         <Query query={GET_ARTICLE} variables={{ slug: router.query.slug }}>
-            {({ loading, data, error }) => <pre>{JSON.stringify({ loading, data, error }, null, 2)}</pre>}
-        </Query>
-        <div className='article-page'>
-            <div className='banner'>
-                <div className='container'>
-                    <h1>How to build webapps that scale</h1>
-                    <div className='article-meta'>
-                        <a href=''><img src='http://i.imgur.com/Qr71crq.jpg' /></a>
-                        <div className='info'>
-                            <a href='' className='author'>Eric Simons</a>
-                            <span className='date'>January 20th</span>
+            {({ data: { article } = {}, loading, error }) => (<>
+                {!loading && !error && <>
+                    <div className='article-page'>
+                        <div className='banner'>
+                            <div className='container'>
+                                <h1>{article.title}</h1>
+                                <div className='article-meta'>
+                                    <a href=''><img src={article.author.image} /></a>
+                                    <div className='info'>
+                                        <a href='' className='author'>{article.author.username}</a>
+                                        <span className='date'>January 20th</span>
+                                    </div>
+                                    <button className='btn btn-sm btn-outline-secondary'>
+                                        <i className='ion-plus-round'></i> Follow {article.author.username} <span className='counter'>(10)</span>
+                                    </button>
+                                    {'  '}
+                                    <button className='btn btn-sm btn-outline-primary'>
+                                        <i className='ion-heart'></i> Favorite Post <span className='counter'>(29)</span>
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <button className='btn btn-sm btn-outline-secondary'>
-                            <i className='ion-plus-round'></i> Follow Eric Simons <span className='counter'>(10)</span>
-                        </button>
-                        {'  '}
-                        <button className='btn btn-sm btn-outline-primary'>
-                            <i className='ion-heart'></i> Favorite Post <span className='counter'>(29)</span>
-                        </button>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div className='container page'>
-            <div className='row article-content'>
-                <div className='col-md-12'>
-                    <p>
-                        Web development technologies have evolved at an incredible clip over the past few years.
-                    </p>
-                    <h2 id='introducing-ionic'>Introducing RealWorld.</h2>
-                    <p>It{'\''}s a great solution for learning how other frameworks work.</p>
-                </div>
-            </div>
-        </div>
+                    <div className='container page'>
+                        <div className='row article-content'>
+                            <div
+                                className='col-md-12'
+                                dangerouslySetInnerHTML={{
+                                    __html: marked(article.body, { santize: true })
+                                }}
+                            />
+                        </div>
+                    </div>
+                </>}
+            </>)}
+        </Query>
     </Layout>
 ));
